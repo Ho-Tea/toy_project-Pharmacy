2b46af3b8091924e1a0e3273d1c8cabc805e6831
yoonjuho <dbswn990@naver.com>
2023-04-19 11:02:38 +0900

    Feat: DirectionService

---
+++ pharmacy/src/main/java/project/pharmacy/direction/service/DirectionService.java
@@ -0,0 +1,67 @@
+package project.pharmacy.direction.service;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import project.pharmacy.api.dto.DocumentDto;
+import project.pharmacy.direction.entity.Direction;
+import project.pharmacy.pharmacy.dto.PharmacyDto;
+import project.pharmacy.pharmacy.service.PharmacySearchService;
+import project.pharmacy.pharmacy.service.PharmacyService;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class DirectionService {
+
+    private static final int MAX_SEARCH_COUNT = 3;  //약국 최대 검색 갯수
+    private static final double RADIUS_KM = 10.0; //반경 10km
+    private final PharmacySearchService pharmacySearchService;
+
+    public List<Direction> buildDirectionList(DocumentDto documentDto){
+
+        if(Objects.isNull(documentDto)) return Collections.emptyList();
+
+
+        //약국 데이터 조회
+        return pharmacySearchService.searchPharmacyDtoList()
+                .stream().map(pharmacyDto ->
+                        Direction.builder()
+                                .inputAddress(documentDto.getAddressName())
+                                .inputLatitude(documentDto.getLatitude())
+                                .inputLongitude(documentDto.getLongitude())
+                                .targetPharmacyName(pharmacyDto.getPharmacyName())
+                                .targetAddress(pharmacyDto.getPharmacyAddress())
+                                .targetLatitude(pharmacyDto.getLatitude())
+                                .targetLongitude(pharmacyDto.getLongitude())
+                                .distance(
+                                        calculateDistance(documentDto.getLatitude(), documentDto.getLongitude(),
+                                                pharmacyDto.getLatitude(), pharmacyDto.getLongitude())
+                                )
+                                .build())
+                .filter(direction -> direction.getDistance() <= RADIUS_KM)
+                .sorted(Comparator.comparing(Direction::getDistance))
+                .limit(MAX_SEARCH_COUNT)
+                .collect(Collectors.toList());
+
+
+    }
+
+    private double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+        lat1 = Math.toRadians(lat1);
+        lon1 = Math.toRadians(lon1);
+        lat2 = Math.toRadians(lat2);
+        lon2 = Math.toRadians(lon2);
+
+        double earthRadius = 6371; //Kilometers
+        return earthRadius * Math.acos(Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2));
+    }
+
+}
