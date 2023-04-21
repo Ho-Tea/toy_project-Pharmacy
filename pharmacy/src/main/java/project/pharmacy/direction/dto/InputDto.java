5f77bc8db0d1bf76d0e16ec0ca064532988f751a
yoonjuho <dbswn990@naver.com>
2023-04-21 17:31:57 +0900

    Feat: DTO

---
+++ pharmacy/src/main/java/project/pharmacy/direction/dto/InputDto.java
@@ -0,0 +1,12 @@
+package project.pharmacy.direction.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+public class InputDto {
+    private String address;
+}
