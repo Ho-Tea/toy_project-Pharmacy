281c42d9c793bbfcbb3bb9d9c8f41d70c6e97d49
yoonjuho <dbswn990@naver.com>
2023-04-24 18:27:14 +0900

    Feat: Redis 초기 데이터 세팅

---
+++ pharmacy/src/main/java/project/pharmacy/pharmacy/controller/PharmacyController.java
@@ -0,0 +1,41 @@
+package project.pharmacy.pharmacy.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import project.pharmacy.pharmacy.cache.PharmacyRedisTemplateService;
+import project.pharmacy.pharmacy.dto.PharmacyDto;
+import project.pharmacy.pharmacy.repository.PharmacyRepository;
+import project.pharmacy.pharmacy.service.PharmacySearchService;
+import project.pharmacy.pharmacy.service.PharmacyService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class PharmacyController {
+    private final PharmacyService pharmacyService;
+    private final PharmacyRedisTemplateService pharmacyRedisTemplateService;
+
+    //데이터 초기 셋팅을 위한 임시 메서드
+    @GetMapping("/redis/save")
+    public String save(){
+        List<PharmacyDto> pharmacyDtoList = pharmacyService.findAll()
+                .stream().map(pharmacy -> PharmacyDto.builder()
+                        .id(pharmacy.getId())
+                        .pharmacyName(pharmacy.getPharmacyName())
+                        .pharmacyAddress(pharmacy.getPharmacyAddress())
+                        .latitude(pharmacy.getLatitude())
+                        .longitude(pharmacy.getLongitude())
+                        .build())
+                .collect(Collectors.toList());
+
+        pharmacyDtoList.forEach(pharmacyRedisTemplateService::save);
+        return "success";
+    }
+
+}
